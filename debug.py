d1b43e68744cdb795c57595ca1e00e1532bacfc1
davenza <daries100@gmail.com>
2019-07-02 16:58:29 +0200

    Dummy script to test Gaussian fit.

--- debug.py
+++ debug.py
@@ -1,50 +1,48 @@
-# import pandas as pd
-# import numpy as np
-# from pgmpy.estimators import HillClimbSearch, BicScore
-#
-#
-# data = pd.DataFrame(np.random.randint(0, 5, size=(5000, 9)), columns=list('ABCDEFGHI'))
-# # add 10th dependent variable
-#
-# data['J'] = data['A'] * data['B']
-# est = HillClimbSearch(data, scoring_method=BicScore(data))
-# best_model = est.estimate()
-# sorted(best_model.nodes())
-#
-# best_model.edges()
-#
-# # search a model with restriction on the number of parents:
-# est.estimate(max_indegree=1).edges()
-
-# import pandas as pd
-# import numpy as np
-# from pgmpy.estimators import ConstraintBasedEstimator
-#
-# data = pd.DataFrame(np.random.randint(0, 5, size=(2500, 3)), columns=list('XYZ'))
-# data['sum'] = data.sum(axis=1)
-# # print(data)
-# c = ConstraintBasedEstimator(data)
-# model = c.estimate()
-# print(model.edges())
-
-# import pandas as pd
-# from pgmpy.models import BayesianModel
-# from pgmpy.estimators import MaximumLikelihoodEstimator
-#
-# data = pd.DataFrame(data={'A': [0, 0, 1], 'B': [0, 1, 0], 'C': [1, 1, 0]})
-# model = BayesianModel([('A', 'C'), ('B', 'C')])
-# cpd_A = MaximumLikelihoodEstimator(model, data).estimate_cpd('A')
-# print(cpd_A)
-#
-# cpd_C = MaximumLikelihoodEstimator(model, data).estimate_cpd('C')
-# print(cpd_C)
-
+import numpy as np
 import pandas as pd
-from pgmpy.models import BayesianModel
-from pgmpy.estimators import BayesianEstimator
 
-data = pd.DataFrame(data={'A': [0, 0, 1], 'B': [0, 1, 0], 'C': [1, 1, 0]})
-model = BayesianModel([('A', 'C'), ('B', 'C')])
-estimator = BayesianEstimator(model, data)
-cpd_C = estimator.estimate_cpd('C', prior_type="dirichlet", pseudo_counts=[1, 2])
-print(cpd_C)
+from pgmpy.models import LinearGaussianBayesianNetwork
+
+# Example data a -> c <- b
+a = np.random.normal(0, 1, 5000)
+b = np.random.normal(3.2, np.sqrt(1.8), 5000)
+c = -1.2 + 2.3*a + 1.5*b + np.random.normal(0, np.sqrt(0.5), 5000)
+
+parents = sorted(['a', 'b'])
+node = 'c'
+data = pd.DataFrame( {'a': a, 'b': b, 'c': c})
+
+def cpd_cov(data):
+    cov = data.cov()
+
+    betas = np.empty((3,))
+
+    sigma_xx_inv = np.linalg.inv(cov.loc[parents, parents])
+    # sigma_xx_inv = pd.DataFrame(np.linalg.inv(cov), cov.columns, cov.index)
+    # sigma_xx_inv = sigma_xx_inv.loc[parents, parents]
+    betas[0] = data[node].mean() - np.dot(cov.loc[node, parents], sigma_xx_inv).dot(data[parents].mean())
+    betas[1:] = np.dot(sigma_xx_inv, cov.loc[node, parents])
+    sigma = cov.loc[node, node] - np.dot(cov.loc[node, parents], sigma_xx_inv).dot(cov.loc[parents, node])
+
+    return betas, sigma
+    # print("cpd_cov():")
+    # print("Betas: " + str(betas))
+    # print("sigma: " + str(sigma))
+
+def cpd_lstqr(data):
+
+    lindata = np.column_stack((np.ones(data.shape[0]), data[parents].values))
+    (betas, res, _, _) = np.linalg.lstsq(lindata, data[node])
+    sigma = res / (data.shape[0] -1)
+
+    return betas, sigma
+    # print("cpd_lstqr():")
+    # print("Betas: " + str(betas))
+    # print("sigma: " + str(sigma))
+
+
+if __name__ == '__main__':
+    model = LinearGaussianBayesianNetwork([('a', 'c'), ['b', 'c']])
+    model.fit(data)
+    for cpd in model.get_cpds():
+        print(cpd)
