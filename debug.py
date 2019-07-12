6e97a7e0d4172cc3b1d3a831244b4efa23e9919d
davenza <daries100@gmail.com>
2019-07-12 10:26:10 +0200

    Changes to debug.py to test Hill Climbing.

--- debug.py
+++ debug.py
@@ -3,10 +3,7 @@ import pandas as pd
 
 import networkx as nx
 
-from pgmpy.models import LinearGaussianBayesianNetwork
-from pgmpy.factors.continuous import LinearGaussianCPD
-from pgmpy.estimators.CachedHillClimbing import CacheDecomposableScore, K2Score
-from pgmpy.estimators.GaussianBicScore import GaussianBicScore
+from pgmpy.estimators import CachedHillClimbing, GaussianBicScore
 
 # Example data a -> c <- b
 a = np.random.normal(0, 1, 5000)
@@ -15,17 +12,54 @@ c = -1.2 + 2.3*a + 1.5*b + np.random.normal(0, np.sqrt(0.5), 5000)
 
 parents = sorted(['a', 'b'])
 node = 'c'
-data = pd.DataFrame( {'a': a, 'b': b, 'c': c})
+data = pd.DataFrame({'a': a, 'b': b, 'c': c})
 data_train = data.iloc[:3000]
 data_predict = data.drop('c', axis=1, inplace=False).iloc[3000:]
 
+data_discrete = pd.DataFrame(np.random.randint(0, 5, size=(5000, 9)), columns=list('ABCDEFGHI'))
 
+ecoli_data = pd.read_csv('ecoli_data.csv')
+ecoli_data.set_index("index", inplace=True)
 
+def to_bnlearn_str(graph):
+
+    for node in graph.nodes:
+        print("[" + node, end='')
+        parents = graph.get_parents(node)
+        if parents:
+            print("|" + parents[0], end='')
+            for parent in parents[1:]:
+                print(":" + parent, end='')
+        print("]", end='')
+
+    print()
 
 if __name__ == '__main__':
-    score = GaussianBicScore(data)
 
-    score.local_score('c', ['a', 'b'])
+    # hc = CacheDecomposableHillClimbing(data)
+    # network = hc.estimate()
+    #
+    # print(network.edges)
+    #
+    # hc_ecoli = CacheDecomposableHillClimbing(ecoli_data, priority_list=[("asnA", "ygcE"),
+    #                                                                 ("aceB", "icdA"),
+    #                                                                 ("hupB", "yfiA"),
+    #                                                                 ("dnaG", "ycgX"),
+    #                                                                 ("lacA", "lacY"),
+    #                                                                 ("lacA", "lacZ"),
+    #                                                                 ("cspG", "yaeM"),
+    #                                                                 ("dnaJ", "sucA"),
+    #                                                                 ("pspA", "pspB")])
+
+    # hc_ecoli = HillClimbSearch(ecoli_data, scoring_method=GaussianBicScore(ecoli_data))
+    # network = hc_ecoli.estimate()
+    hc_ecoli = CachedHillClimbing(ecoli_data)
+    network = hc_ecoli.estimate()
+    to_bnlearn_str(network)
+    print(network.edges)
+
+    print("Num edges: " + str(len(network.edges)))
+
 
     # G = nx.DiGraph([(0,1), (0,2), (1,2)])
     # cached = CacheDecomposableScore(G, K2Score, [], [])
