ab98db3caa022ae14e60b73c1be91fa5f71d3e24
davenza <daries100@gmail.com>
2019-07-26 18:06:40 +0200

    Updated debug.py

--- debug.py
+++ debug.py
@@ -5,6 +5,9 @@ import pandas as pd
 import networkx as nx
 
 from pgmpy.estimators import CachedHillClimbing, GaussianBicScore, BGeScore
+from pgmpy.estimators.BGeScorePy import BGeScore as BGeScorePy
+
+import timeit
 
 # Example data a -> c <- b
 a = np.random.normal(0, 1, 5000)
@@ -35,52 +38,19 @@ def to_bnlearn_str(graph):
 
     print()
 
-if __name__ == '__main__':
 
-    # hc = CacheDecomposableHillClimbing(data)
-    # network = hc.estimate()
-    #
+
+
+if __name__ == '__main__':
+    bge = BGeScore(ecoli_data)
+    bge.local_score("asnA", ["aceB"])
+
+    # bge.local_score("aceB", ["atpD", "b1583", "b1963", "cspG", "dnaG", "dnaJ", "dnaK", "eutG", "fixC", "flgD"])
+    # hc_ecoli = CachedHillClimbing(ecoli_data, scoring_method=BGeScore(ecoli_data))
+    # network = hc_ecoli.estimate()
+    # to_bnlearn_str(network)
     # print(network.edges)
     #
-    # hc_ecoli = CacheDecomposableHillClimbing(ecoli_data, priority_list=[("asnA", "ygcE"),
-    #                                                                 ("aceB", "icdA"),
-    #                                                                 ("hupB", "yfiA"),
-    #                                                                 ("dnaG", "ycgX"),
-    #                                                                 ("lacA", "lacY"),
-    #                                                                 ("lacA", "lacZ"),
-    #                                                                 ("cspG", "yaeM"),
-    #                                                                 ("dnaJ", "sucA"),
-    #                                                                 ("pspA", "pspB")])
-
-    # hc_ecoli = HillClimbSearch(ecoli_data, scoring_method=GaussianBicScore(ecoli_data))
-    # network = hc_ecoli.estimate()
-    hc_ecoli = CachedHillClimbing(ecoli_data, scoring_method=BGeScore(ecoli_data))
-    network = hc_ecoli.estimate()
-    to_bnlearn_str(network)
-    print(network.edges)
-
-    print("Num edges: " + str(len(network.edges)))
+    # print("Num edges: " + str(len(network.edges)))
 
 
-    # G = nx.DiGraph([(0,1), (0,2), (1,2)])
-    # cached = CacheDecomposableScore(G, K2Score, [], [])
-    #
-    # G = nx.DiGraph([(0,1), (1,2)])
-    # cached = CacheDecomposableScore(G, K2Score, [], [])
-    #
-    # G = nx.DiGraph([(0,2), (1,2), (0,1), (3,1), (3,4), (4,5), (4,6), (5,7), (6,7), (0,5)])
-    # cached = CacheDecomposableScore(G, K2Score, [], [])
-
-    # model = LinearGaussianBayesianNetwork([('a', 'c'), ('b', 'c')])
-    # cpd1 = LinearGaussianCPD('a', [1], 4)
-    # cpd2 = LinearGaussianCPD('b', [-5], 4)
-    # cpd3 = LinearGaussianCPD('c', [4, -1, 7], 3, ['a', 'b'])
-    # model.fit(data_train)
-    #
-    # for cpd in model.get_cpds():
-    #     print(cpd)
-    #
-    # print(model.to_joint_gaussian().covariance)
-    # prediction = model.predict(data_predict)
-    # print(prediction)
-    # print((data['c'].iloc[3000:] - prediction['c']).var())
